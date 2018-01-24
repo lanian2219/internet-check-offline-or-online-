7bcee937438917623dca87dd47c982608178f8c5
lanian2219 <lanian2219@github>
2018-01-24 22:05:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# internet-check--offline-or-online-
---
+++ script-linux.py
@@ -0,0 +1,17 @@
+# -*- encoding: iso-8859-15 -*-
+
+
+import os
+from time import sleep
+
+hostname = "google.com"
+
+while True:
+
+    response = os.system("ping -c 1 " + hostname)
+    if response == 1:
+        print("No Internet")
+    else:
+        print("Internet!")
+        pass
+    sleep(60)
